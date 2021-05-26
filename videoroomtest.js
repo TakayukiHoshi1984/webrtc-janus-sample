64311a7a6be0066e7c1732e04d91d4802a8f8a2a
TakayukiHoshi1984 <hoshi@gclue.jp>
2021-05-26 16:45:10 +0900

    change to videoroomtest.js

--- videoroomtest.js
+++ videoroomtest.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
+	server = "http://127.0.0.1:8088/janus";
 else
-	server = "https://" + window.location.hostname + ":8089/janus";
+	server = "https://127.0.0.1:8089/janus";
 
 var janus = null;
 var sfutest = null;
