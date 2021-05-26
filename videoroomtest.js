fc54c1bc1bc000e5fa39d261f2bca7ccefdd9b71
TakayukiHoshi1984 <hoshi@gclue.jp>
2021-05-26 16:54:40 +0900

    change ip

--- videoroomtest.js
+++ videoroomtest.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://127.0.0.1:8088/janus";
+	server = "http://localhost:8088/janus";
 else
-	server = "https://127.0.0.1:8089/janus";
+	server = "https://localhost:8089/janus";
 
 var janus = null;
 var sfutest = null;
